dfa8c50caae7bbdc13e4c9255113e6db3243bb38
Arina Grushchenko <arinagrushchenko@gmail.com>
2022-08-05 11:09:24 +0300

    savedData obj

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,30 +3,32 @@ const STORAGE_KEY = 'feedback-form-state';
 const refs = {
   form: document.querySelector('.feedback-form'),
 };
-const formData = {};
 
 const onFormSubmit = e => {
-  e.preventDefault();
-  const { email, message } = e.currentTarget.elements;
-  const userData = {
-    email: email.value,
-    message: message.value,
-  };
-  console.log(userData);
+    e.preventDefault();
+    
+    const { email, message } = e.currentTarget.elements;
+    const userData = {
+      email: email.value,
+      message: message.value,
+    };
+    console.log(userData);
+    
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 };
 
 const onFormInput = e => {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  let persistedData = localStorage.getItem(STORAGE_KEY);
+  persistedData = persistedData ? JSON.parse(persistedData) : {};
+  persistedData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedData));
 };
 
 function fillInputs() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (savedData) {
     for (const key in savedData) {
-      formData[key] = savedData[key];
       refs.form.elements[key].value = savedData[key];
     }
   }
